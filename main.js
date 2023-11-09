77a39aa7ccfa898cf143bb18a447b359295f700f
fultlin <ugolok.kita@gmail.com>
2023-11-09 14:00:11 +0300

    grid

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert('Привет мир!')
+// let name = "jhon";
+// const COLOR_GREEN = "#0F0";
+// const MyBiryhday = "17.12.2005";
+// alert(`Hello, ${name}`)
+// alert(COLOR_GREEN);
+// alert(MyBiryhday);
+// let num = 123;
+// num = 10.23;
+// console.log(num);
+result = prompt('QQ');
+let isBoss = confirm("Ты здесь главный?");
+alert( isBoss );
+console.log(typeof(x))
