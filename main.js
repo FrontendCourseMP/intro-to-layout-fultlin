dafb11e1958fc09d40b5452a3cd98f716670bb28
fultlin <ugolok.kita@gmail.com>
2023-11-13 19:04:11 +0300

    mobile

--- main.js
+++ main.js
@@ -7,7 +7,29 @@
 // let num = 123;
 // num = 10.23;
 // console.log(num);
-result = prompt('QQ');
-let isBoss = confirm("Ты здесь главный?");
-alert( isBoss );
-console.log(typeof(x))
+// result = prompt('QQ');
+// let isBoss = confirm("Ты здесь главный?");
+// alert( isBoss );
+// console.log(typeof(x))
+
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.button-prev');
+const nextButton = document.querySelector('.button-next');
+const slides = Array.from(slider.querySelectorAll('.slider__content'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+function next() {
+    var slider = document.querySelector('.slider');
+    var slides = slider.querySelectorAll('.slider');
+
+    if (slideIndex < slides.length - 1) {
+        slides[slideIndex].style.display = 'none';
+        slideIndex++;
+        slides[slideIndex].style.display = 'block';
+    } else {
+        slideIndex = 0;
+        slides[slides.length - 1].style.display = 'none';
+        slides[slideIndex].style.display = 'block';
+    }
+}
